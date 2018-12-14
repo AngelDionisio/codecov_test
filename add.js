4ea54fbcebf63e38c5569b837a244736897215e0
Angel Dionisio <angel.dionisio@Angels-MacBook-Pro.local>
2018-12-14 15:56:15 -0500

    adding another not covered function

--- add.js
+++ add.js
@@ -2,10 +2,14 @@ const adders = {
     add: (x, y) => {
         return x + y;
     },
-    
+
     arrayAdder: (arr) => {
         return arr.reduce(add);
-    }    
+    },
+
+    addOne: (x) => {
+        return x + 1;
+    }
 }
 
 module.exports = adders;
