0caf878fe9feeb32c1f65269370ea9be00d140af
Angel Dionisio <angel.dionisio@Angels-MacBook-Pro.local>
2018-12-14 15:54:53 -0500

    work in progress

--- add.js
+++ add.js
@@ -9,16 +9,3 @@ const adders = {
 }
 
 module.exports = adders;
-
-// function add(x, y) {
-//     return x + y;
-// }
-
-// function arrayAdder(arr) {
-// 	return arr.reduce(add);
-// }
-
-// module.exports = {
-//     add,
-//     arrayAdder
-// };
